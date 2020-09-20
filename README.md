d38df9082c8b8bd9123f8c6f9df2b371e7669e6e
Dugald Steer <59143581+Dugsteer@users.noreply.github.com>
2020-09-20 14:57:42 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+This is the Bingo Caller for SPORT
---
+++ script.js
@@ -0,0 +1,108 @@
+
+
+//GENERAL SCRIPT
+const bingoArray = [
+  "archery.svg", "badminton.svg", "basketball.svg", "boxing.svg", "climbing.svg", "cycling.svg", "diving.svg", "driving.svg", "football.svg", "golf.svg", "gymnastics.svg", "hang-gliding.svg", "hiking.svg", "hockey.svg", "horse-riding.svg", "ice-skating.svg", "karate.svg", "kayak.svg", "paddle-surf.svg", "rugby.svg", "running.svg", "sailing.svg", "skiing.svg", "surfing.svg", "swimming.svg", "table-tennis.svg", "tennis.svg", "weight-lifting.svg", "windsurfing.svg", "yoga.svg"
+  ];
+
+  const wrapper = document.getElementById("wrapper");
+  const newArray = [];
+  
+  function shuffle(bingoArray) {
+    var j, x, i;
+    for (i = bingoArray.length - 1; i > 0; i--) {
+      j = Math.floor(Math.random() * (i + 1));
+      x = bingoArray[i];
+      bingoArray[i] = bingoArray[j];
+      bingoArray[j] = x;
+    }
+    return bingoArray;
+  }
+  
+  const button0 = document.getElementById("button0");
+  const button1 = document.getElementById("button1");
+  const button2 = document.getElementById("button2");
+  const clicker2 = document.getElementById("clicker2");
+  
+  function bingoCall() {
+    button1.style.display = "none";
+    button2.style.display = "none";
+    wrapper.classList.add("unCenter");
+    wrapper.innerHTML = "";
+    const oldArray = bingoArray;
+    var blue = oldArray.shift();
+    clicker2.innerHTML = oldArray.length > 0 ? `${oldArray.length} to go` : `END`;
+    newArray.push(blue);
+    fillHTML();
+  }
+  
+  function bingoCall1() {
+    button0.style.display = "none";
+    button2.style.display = "none";
+    wrapper.classList.add("unCenter");
+    wrapper.innerHTML = "";
+    const oldArray = bingoArray;
+    var blue = oldArray.shift();
+    clicker2.innerHTML = oldArray.length > 0 ? `${oldArray.length} to go` : `END`;
+    newArray.push(blue);
+    fillHTML1();
+    }
+  
+  
+  function bingoCall2() {
+    button1.style.display = "none";
+    button0.style.display = "none";
+    wrapper.classList.add("unCenter");
+    wrapper.innerHTML = "";
+    const oldArray = bingoArray;
+    var blue = oldArray.shift();
+    clicker2.innerHTML = oldArray.length > 0 ? `${oldArray.length} to go` : `END`;
+    newArray.push(blue);
+    fillHTML2();
+  }
+  
+  function fillHTML() {
+    // for (var i=0; i<newArray.length;i++){
+    //   wrapper.innerHTML =
+    newArray.forEach(function (item) {
+      var div = document.createElement("div");
+      div.innerHTML = `
+    <div class="minicard text-center">
+    <img src="./img/${item}">
+    <h3 class="minicard__h3">${item.slice(0, -4)}</h3>
+  </div>
+  `;
+      wrapper.appendChild(div);
+    });
+  }
+  
+  function fillHTML1() {
+    newArray.forEach(function (item) {
+      let div = document.createElement("div");
+      (item.length < 2) ? console.log('no') : 
+      div.innerHTML = `
+    <div class="minicard text-center d-flex justify-content-center">
+    <img src="./img/${item}">
+  </div>
+  `;
+      wrapper.appendChild(div);
+    });
+  }
+  
+  
+  function fillHTML2() {
+    // for (var i=0; i<newArray.length;i++){
+    //   wrapper.innerHTML =
+    newArray.forEach(function (item) {
+      var div = document.createElement("div");
+      div.innerHTML = `
+    <div class="minicard text-center d-flex justify-content-center">
+    <h3 class="minicard__h3">${item.slice(0, -4)}</h3>
+  </div>
+  `;
+      wrapper.appendChild(div);
+    });
+  }
+  
+  
+  shuffle(bingoArray);
